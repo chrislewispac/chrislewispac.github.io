3a5b0763fd664aeb029bb31470f3d31534b7b035
Chris Lewis <chrislewispac@gmail.com>
2020-07-25 12:30:22 -0700

    add two data sets for exploration

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+async function init() {
+    const data = await d3.json('./trumptweets.json');
+    // console.log(data)
+    let text = data.map((d) => {
+        return d.text
+    })
+    let justMentions = text.filter((t) => {
+
+    })
+}
+
+init()
