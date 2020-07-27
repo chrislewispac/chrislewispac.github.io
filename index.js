41db6af5e3ae34ce4419e97c34a33d4864ec2a7f
Chris Lewis <chrislewispac@gmail.com>
2020-07-27 13:03:32 -0700

    mock bar charts to sanity check

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-async function init() {
-    const data = await d3.json('./trumptweets.json');
-    // console.log(data)
-    let text = data.map((d) => {
-        return d.text
-    })
-    let justMentions = text.filter((t) => {
+// async function init() {
+//     const data = await d3.json('./trumptweets.json');
+//     // console.log(data)
+//     let text = data.map((d) => {
+//         return d.text
+//     })
+//     let justMentions = text.filter((t) => {
 
-    })
-}
+//     })
+// }
 
-init()
+// init()
